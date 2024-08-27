c7eaffb3df4d2d596b79547cf8286028d18846c6
venkat-095 <2320090079@klh.edu.in>
2024-08-27 10:40:47 +0530

    Delete Task 6A-i directory

--- Task 6A-i/src/taskmanagement/Task.java
+++
@@ -1,8 +0,0 @@
-package taskmanagement;
-
-public class Task {
-    private String description;
-    public Task(String description) { this.description = description; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-}
